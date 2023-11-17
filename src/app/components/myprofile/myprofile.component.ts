968162d10bbe0145fb0abd5e3d32927d4ff23b59
debdoit <debasishp@smbxl.com>
2023-11-17 15:35:11 +0530

    update

--- src/app/components/myprofile/myprofile.component.ts
+++ src/app/components/myprofile/myprofile.component.ts
@@ -25,114 +25,40 @@ export class MyprofileComponent {
   public email: string = "";
   public role!:string;
   public Username!:string;
-  public FirstName!:string;
+  public firstName!:string;
   public LastName!:string;
   public Email!:string;
   public Contact!:string;
   public Id!:string;
+  user: any; // Define a user variable to store the fetched user details
    
   // public Id: string = "";
   constructor(private auth: AuthService, private userStore: UserStoreService, private route: ActivatedRoute, private modalService: NgbModal,private GetcustomerdetailsService: GetcustomerdetailsService,private http: HttpClient) { }
- 
   ngOnInit() {
-    this.userStore.getFullNameFromStore().subscribe(fullName => {
-      console.log('Full Name from Store:', fullName);
-      
-      if (fullName) {
-        
-        this.fullName = fullName;
-      } else {
-        this.fullName = this.auth.getfullNameFromToken();
-      }
-      console.log('Full Name in Component:', this.fullName);
-      
-    });
-
-   
-
-    this.userStore.getUsernameFromStore()
-    .subscribe(val=>{
-      console.log('Username from store:', val);
-
-      const UsernameFromToken = this.auth.getUsernameFromToken();
-      this.Username = val || UsernameFromToken;
-
-      console.log('Username From Store:', this.Username );
-    });
-
-   
-
-    this.userStore.getRoleFromStore()
-    .subscribe(val=>{
-      console.log('Role Store:', val);
-      const roleFromToken = this.auth.getRoleFromToken();
-      this.role = val || roleFromToken;
-
-      console.log('Role From Store:', this.role );
-    });
-
-
-    this.userStore.getEmailFromStore()
-    .subscribe(val=>{
-      console.log('Email Store:', val);
-      const EmailFromToken = this.auth.getEmailFromToken();
-      this.Email = val || EmailFromToken;
-
-      console.log('Email From Store:', this.Email );
-    });
-
-
-    this.userStore.getLastnameFromStore()
-    .subscribe(val=>{
-      console.log('Lastname Store:', val);
-      const LastnameFromToken = this.auth.getLastnameFromToken();
-      this.LastName = val || LastnameFromToken;
-
-      console.log('Lastname From Store:', this.LastName );
-    });
-
-
-
-    this.userStore.getFirstnameFromStore()
-    .subscribe(val=>{
-      console.log('Firstname Store:', val);
-      const FirstnameFromToken = this.auth.getFirstnameFromToken();
-      this.FirstName = val || FirstnameFromToken;
-
-      console.log('Firstname From Store:', this.FirstName );
-    });
-
-
-    this.userStore.getContact_NoFromStore()
-    .subscribe(val=>{
-      console.log('Contact Store:', val);
-      const contactnoFromToken = this.auth.getcontactnoFromToken();
-      this.Contact = val || contactnoFromToken;
-
-      console.log('Contact_No From Store:', this.Contact );
-    });
-
-
-    this.userStore.getIdFromStore()
-    .subscribe(val=>{
+    this.userStore.getIdFromStore().subscribe(val => {
       console.log('Id Store:', val);
       const IdFromToken = this.auth.getIdFromToken();
       this.Id = val || IdFromToken;
 
-      console.log('Id From Store:', this.Id );
+      console.log('Id From Store:', this.Id);
+
+      // Move the following code inside the subscribe block to ensure Id is assigned before using it
+      this.GetcustomerdetailsService.getBOUSERDetails(this.Id).subscribe((data) => {
+        this.user = data;
+        console.log(data);
+      });
     });
-
   }
-
-
+  
 
   updateProfile() {
+    console.log("username",this.user.firstName);
     // Prepare the updated user data to send to the server
     const updatedUser = {
-      firstName: this.FirstName,
-      lastName: this.LastName,
-      contact: this.Contact,
-      email: this.Email,
+      firstName: this.user.firstName,
+      lastName: this.user.lastName,
+      contact: this.user.contact,
+      email: this.user.email,
     };
   
     // Assuming 'id' is the identifier of the user you want to update
